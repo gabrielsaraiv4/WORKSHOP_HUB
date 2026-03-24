4048a56c05e02e46d23f9ef9786bfa5885776d1c
Gabriel Saraiva <gabrielsaraiva1502@gmail.com>
2026-03-24 12:58:46 -0300

    feat: sidebar layout and routes management

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { WorkshopListComponent } from './components/workshop-list/workshop-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: WorkshopListComponent },
+    { path: 'workshops', component: WorkshopListComponent }
+];
---
+++ client/src/app/services/workshop.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Workshop } from '../models/workshop.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class WorkshopService {
+    private apiUrl = 'http://localhost:5013/api/workshops';
+
+    constructor(private http: HttpClient) { }
+
+    getWorkshops(): Observable<Workshop[]> {
+        return this.http.get<Workshop[]>(this.apiUrl);
+    }
+
+    getWorkshopById(id: number): Observable<Workshop> {
+        return this.http.get<Workshop>(`${this.apiUrl}/${id}`);
+    }
+}
